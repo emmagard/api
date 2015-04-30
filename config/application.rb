b8480986355057f11ec0115c33f6d4c0b46f189d
Emma Gardner <emmagardner@outlook.com>
2015-04-30 13:56:04 -0400

    stufff

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,13 @@ module DxApi
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    # config.active_record.raise_in_transactional_callbacks = true
+
+    config.middleware.insert_before "ActionDispatch::Static", "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
+      end
+    end
   end
 end
